4399b7de48d4c1fbdc7ab6f669f60c4e2d7f1e0b
yn-03 <yn-03@LAPTOP-ND1LRF0N>
2019-06-14 15:13:13 +0900

    2

---
+++ 6장/src/test/Animal.java
@@ -0,0 +1,9 @@
+package test;
+public class Animal {
+	public static void testClassMethod() { //클래스 메소드인 testClassMethod 정의
+		System.out.println("The class method in Animal.");
+	}
+	public void testInstanceMethod() { //슈퍼클래스에서 인스턴트메소드인 testInstanceMethd 정의
+		System.out.println("The instance method in animal.");
+	}
+}
